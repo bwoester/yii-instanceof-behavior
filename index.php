685ad2e79335fd127cb47e3c8568294d55119815
libra guest <benjamin.woester@gmail.com>
2012-08-24 17:30:31 -0400

    make index.php redirect to the yii application

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-
-    phpinfo();
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: sampleApp/');
+?>
+<html>
+    <head>
+        <title>Moved</title>
+    </head>
+    <body>
+        <h1>Moved</h1>
+        <p>This page has moved to <a href="sampleApp/">sampleApp</a>.</p>
+    </body>
+</html>

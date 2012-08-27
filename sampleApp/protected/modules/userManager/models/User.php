c5bc14057c893a9fa5b47f1c240492b01373ec2a
Benjamin Woester <benjamin.woester@gmail.com>
2012-08-28 01:12:07 +0200

    use different image in demop app

--- sampleApp/protected/modules/userManager/models/User.php
+++ sampleApp/protected/modules/userManager/models/User.php
@@ -8,7 +8,7 @@
 class User
 {
   public $email = 'testuser@domain.com';
-  public $image = 'http://image.shutterstock.com/display_pic_with_logo/274327/274327,1317563799,14/stock-vector-profile-icon-85771417.jpg';
+  public $image = 'http://placehold.it/260x180';
   public $id = 42;
   public $name = 'UserManager User';
 }
